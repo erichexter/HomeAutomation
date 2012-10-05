2b69ca3313c375671aabd007ab1230bb386db5c0
Eric Hexter <eric.hexter@gmail.com>
2012-10-05 16:50:04 -0500

    added event to scene execution
    a scene with the same name as an event will cause it to trigger.

--- src/HomeAutomation/UI/Hub.cs
+++ src/HomeAutomation/UI/Hub.cs
@@ -1,19 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using SignalR.Hubs;
 using UI.Controllers;
 
 namespace HomeAutomation
 {
-    public class RelayCommands:SignalR.Hubs.Hub
+    public class RelayCommands : Hub
     {
-        DeviceRepository repository = new DeviceRepository();
-        SceneRepository sceneRepository=new SceneRepository();
+        private readonly DeviceRepository repository = new DeviceRepository();
+        private readonly SceneRepository sceneRepository = new SceneRepository();
+
         public void SendCommandToClient(string address, string command)
         {
             Clients.ExecuteCommand(address, command);
@@ -21,38 +19,48 @@ namespace HomeAutomation
 
         public void TimedEvent(string eventname)
         {
-            Clients.eventFired(eventname);
+            Scene scene =
+                sceneRepository.GetAll().FirstOrDefault(
+                    e => e.DisplayName.Replace(" ","").Equals(eventname, StringComparison.InvariantCultureIgnoreCase));
+            if (scene != null)
+            {
+                RunScene(scene.Value);
+                Clients.eventFired(eventname);
+            }
         }
 
         public void programNewDevice(string address)
         {
             Clients.program(address);
         }
+
         public void CommandSent(string address, string command)
         {
-            var device = repository.Get(address);
+            Device device = repository.Get(address);
             if (device != null)
             {
                 device.State = command;
             }
-            var id = device!=null?device.DisplayName:address;
-            Clients.sent(id,command);
+            string id = device != null ? device.DisplayName : address;
+            Clients.sent(id, command);
         }
+
         public void CommandReceived(string address, string command)
         {
             //received a command from RF / motion sensor / remote control.
         }
+
         public void HeartBeat(DateTime datetime, string message)
         {
-            Clients.heartBeatReceived(datetime.ToString(),message);
+            Clients.heartBeatReceived(datetime.ToString(), message);
         }
 
         public void RunScene(int sceneId)
         {
-            var scene = sceneRepository.Get(sceneId);
-            var js = Newtonsoft.Json.JsonSerializer.Create(new Newtonsoft.Json.JsonSerializerSettings());
-            StringWriter jw = new StringWriter();
-            js.Serialize(jw,scene);
+            Scene scene = sceneRepository.Get(sceneId);
+            JsonSerializer js = JsonSerializer.Create(new JsonSerializerSettings());
+            var jw = new StringWriter();
+            js.Serialize(jw, scene);
             Clients.runSceneOnAgent(jw.ToString());
         }
     }
